efc03d45f6387977cf03a771b2d2373ae5c4b458
Asun Gomez <asun.gomez@wazuh.com>
2020-05-20 15:47:48 +0200

    Add redirections validations

--- app/models/redirection.rb
+++ app/models/redirection.rb
@@ -1,4 +1,16 @@
 class Redirection < ApplicationRecord
   belongs_to :origin, class_name: 'Page', foreign_key: 'from', inverse_of: :origin_redirections
   belongs_to :destination, class_name: 'Page', foreign_key: 'to', inverse_of: :destination_redirections
+
+  validates :from, presence: true
+  validates :to, presence: true, uniqueness: {scope: :from}
+  validate :destinations_must_be_from_different_branches
+
+  def destinations_must_be_from_different_branches
+    origin = Page.find(from)
+    destination = Page.find(to)
+    if origin.destinations.pluck(:branch_id).include?(destination.branch_id)
+      errors.add(:to, "A page can only have one redirection destination per branch")
+    end
+  end
 end

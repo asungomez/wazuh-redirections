866f31abc2ecd587d4e4409cf843b05086ce54df
Asun Gomez <asun.gomez@wazuh.com>
2020-05-20 15:35:49 +0200

    Set autocomplete suggestions

--- app/javascript/packs/rename_form.js
+++ app/javascript/packs/rename_form.js
@@ -1,3 +1,4 @@
+import autocomplete from "autocompleter";
 
 var destinationPathInput = document.getElementById('destination_page');
 var destinationIdInput = document.getElementById('destination_id');
@@ -5,7 +6,9 @@ var originPathInput = document.getElementById('origin_page');
 var originIdInput = document.getElementById('origin_id');
 
 const destinationsData = JSON.parse(document.getElementById('destinations-section').dataset.destinations);
+const destinationsAutocomplete = filterForAutocomplete(destinationsData);
 const originsData = JSON.parse(document.getElementById('origins-section').dataset.origins);
+const originsAutocomplete = filterForAutocomplete(originsData);
 
 destinationPathInput.addEventListener('change', function(event) {
   const destinationPath = destinationPathInput.value;
@@ -21,3 +24,40 @@ originPathInput.addEventListener('change', function(event) {
   }
 });
 
+autocomplete({
+  input: destinationPathInput,
+  emptyMsg: "No pages found",
+  minLength: 1,
+  fetch: function(text, update) {
+      var suggestions = destinationsAutocomplete.filter(n => n.label.startsWith(text));
+      update(suggestions);
+  },
+  onSelect: function(item) {
+    destinationPathInput.value = item.label;
+  }
+});
+
+autocomplete({
+  input: originPathInput,
+  emptyMsg: "No pages found",
+  minLength: 1,
+  fetch: function(text, update) {
+      var suggestions = originsAutocomplete.filter(n => n.label.startsWith(text));
+      update(suggestions);
+  },
+  onSelect: function(item) {
+    originPathInput.value = item.label;
+  }
+});
+
+function filterForAutocomplete(data) {
+  return Object.keys(data).map(
+    (path) => {
+      const autocompleteItem = {
+        label: path,
+        value: path
+      }
+      return autocompleteItem;
+    }
+  );
+}
